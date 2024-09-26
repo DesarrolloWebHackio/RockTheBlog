115f54a21e1b2413ab1a46b30bec52184f374b16
DesarrolloWebHackio <desarrolloweb@hackio.com>
2024-09-26 12:27:44 +0200

    FEAT animation navlinks bubble

--- src/utils/data/routes.js
+++ src/utils/data/routes.js
@@ -1,21 +1,27 @@
 import { Experiences } from "../../pages/Experiences/Experiences";
+import { Home } from "../../pages/Home/Home";
 import { Projects } from "../../pages/Projects/Projects";
 import { Studies } from "../../pages/Studies/Studies";
 
 export const routes = [
-    {
-        text: "Proyectos",
-        path: "/projects",
-        page: Projects
-    },
-    {
-        text: "Experiencias",
-        path: "/experiences",
-        page: Experiences
-    },
-    {
-        text: "Estudios",
-        path: "/studies",
-        page: Studies
-    },
+  {
+    text: "Home",
+    path: "/",
+    page: Home,
+  },
+  {
+    text: "Proyectos",
+    path: "/projects",
+    page: Projects,
+  },
+  {
+    text: "Experiencias",
+    path: "/experiences",
+    page: Experiences,
+  },
+  {
+    text: "Estudios",
+    path: "/studies",
+    page: Studies,
+  },
 ];
