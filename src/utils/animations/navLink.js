6ada4cac10641fdbe720add560136956561ed089
DesarrolloWebHackio <desarrolloweb@hackio.com>
2024-10-01 10:22:59 +0200

    FEAT studies & experience data && FIX bubbles animations

--- src/utils/animations/navLink.js
+++ src/utils/animations/navLink.js
@@ -3,9 +3,17 @@ import anime from "animejs";
 export const navLinkAnimation = (target, complete) => {
   anime({
     targets: target,
-    borderRadius: 0,
-    duration: 0,
+    keyframes: [
+      {
+        width: "100%",
+        height: "100%",
+        top: 0,
+        left: 0,
+        offset: 0,
+      },
+    ],
+    duration: 600,
     easing: "linear",
-    complete
+    complete,
   });
 };
--- src/utils/data/routes.js
+++ src/utils/data/routes.js
@@ -8,20 +8,28 @@ export const routes = [
     text: "Home",
     path: "/",
     page: Home,
+    top: "20px",
+    left: "50px",
   },
   {
     text: "Proyectos",
     path: "/projects",
     page: Projects,
+    top: "200px",
+    right: "70px",
   },
   {
     text: "Experiencias",
     path: "/experiences",
     page: Experiences,
+    bottom: "100px",
+    left: "200px",
   },
   {
     text: "Estudios",
     path: "/studies",
     page: Studies,
+    top: "40%",
+    left: "30%",
   },
 ];
